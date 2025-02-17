5b84be0a8d98d711f9d7b14988521265d622a858
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-02-17 22:39:35 +0800

    context with deadline

--- context_test.go
+++ context_test.go
@@ -165,3 +165,50 @@ func TestContextWithTimeout(t *testing.T) {
 
 	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
 }
+
+func CreateCounterWithDeadline(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+
+		counter := 1
+
+		for {
+			select {
+			case <- ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+				time.Sleep(1 * time.Second) // simulate slow process
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithDeadline(t *testing.T) {
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+	fmt.Println(time.Now())
+
+	parent := context.Background()
+	ctx, cancel := context.WithDeadline(parent, time.Now().Add(5 * time.Second))
+
+	defer cancel()
+
+	destination := CreateCounterWithDeadline(ctx)
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
+
+	fmt.Println(time.Now())
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+}
