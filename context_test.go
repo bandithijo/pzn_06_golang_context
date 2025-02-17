51b757a239731c5f3f4784a2507f4179d1ec8f38
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2025-02-17 20:11:28 +0800

    context with cancel

--- context_test.go
+++ context_test.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestContext(t *testing.T) {
@@ -36,4 +38,86 @@ func TestContextWithValue(t *testing.T) {
 	fmt.Println(contextF.Value("c")) // dapat milik parent => C
 	fmt.Println(contextF.Value("b")) // tidak dapat, beda parent => nil
 	fmt.Println(contextA.Value("b")) // tidak bisa mengambil data child => nil
+}
+
+func CreateCounter() chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+
+		counter := 1
+
+		for {
+			destination <- counter
+			counter++
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithLeak(t *testing.T) {
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	destination := CreateCounter()
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+
+		if n == 10 {
+			break
+		}
+	}
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+}
+
+func CreateCounterWithContext(ctx context.Context) chan int {
+	destination := make(chan int)
+
+	go func() {
+		defer close(destination)
+
+		counter := 1
+
+		for {
+			select {
+			case <- ctx.Done():
+				return
+			default:
+				destination <- counter
+				counter++
+			}
+		}
+	}()
+
+	return destination
+}
+
+func TestContextWithCancel(t *testing.T) {
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithCancel(parent)
+
+	destination := CreateCounterWithContext(ctx)
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+
+		if n == 10 {
+			break
+		}
+	}
+
+	cancel() // mengirim sinyal cancel ke context
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total Goroutine:", runtime.NumGoroutine())
 }
